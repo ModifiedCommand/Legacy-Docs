32973e20dbb890ab9aa4195558f212df9a1eeb57
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2024-04-14 18:39:59 +0000

    Deploying to gh-pages from @ ModifiedCommand/Legacy-Docs@9df512e7f86e86917e01e7d4ac290682d8a405fe 🚀

---
+++ assets/index.html-0a99b83b.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-12759b28","path":"/Formats/","title":"Home","lang":"en-US","frontmatter":{"title":"Home"},"headers":[],"git":{"updatedTime":1713119974000,"contributors":[{"name":"PiSaucer","email":"33438654+PiSaucer@users.noreply.github.com","commits":1}]},"filePathRelative":"Formats/README.md"}');export{e as data};
